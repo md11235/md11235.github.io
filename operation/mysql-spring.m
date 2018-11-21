5d718f17162a28c02d0c005c0e53defefc63b633
Madsen Zhang <md11235@gmail.com>
2018-11-21 18:31:57 +0800

    Create mysql-spring.m

---
+++ operation/mysql-spring.m
@@ -0,0 +1,7 @@
+# MySQL, Spring
+
+```shell 
+wget https://cdn.mysql.com//Downloads/MySQLInstaller/mysql-installer-community-5.7.24.0.msi
+wget https://archive.apache.org/dist/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz
+# jdk? wget 
+```
